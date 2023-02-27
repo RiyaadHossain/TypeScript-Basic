e0469e164507d74223bd9d0b26bfaed1d4bf64a4
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-27 21:27:52 +0600

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -205,6 +205,7 @@ const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string
   if (c === 'add') return a + b
   return '' + a + b
 }
+let myVal: string = addOrConcat(2, 2, 'concat') as string // To prevent getting error
 
 // The DOM 
 const img = document.querySelector('img')!
