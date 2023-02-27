b1f1f2aabf1a8efb89812f56fe90a79a650fe6a4
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-27 09:11:39 +0600

    Array & Tuples

--- build/js/main.js
+++ build/js/main.js
@@ -18,3 +18,21 @@ const re = /\w+/g;
 const sum = (a, b) => {
     return a + b;
 };
+/* ------------------- 3. Array ------------------- */
+const anyTypeString = [];
+const anyTypeString2 = [];
+const stringArr = ["Hello", "how", "are", "you"];
+const stringArr2 = ["Hello", "how", "are", "you"];
+// X - stringArr.push(34)
+const strAndNum = ["Hi", 143];
+const strAndNum2 = ["Hi", 143];
+// X - strAndNum2[0](boolean)
+const mixedData = ["bye", true, 77];
+const mixedData2 = ["bye", true, 77];
+/* ------------------- 4. Tuple ------------------- */
+const myTuple = ['hello', 143, false];
+// X - const myTuple: [string, number, boolean, number] = ['hello', 143, false]
+let anArr = ['hello1', 234, true];
+myTuple[1] = 777;
+anArr = myTuple;
+// X - myTuple = Array
--- src/main.ts
+++ src/main.ts
@@ -17,11 +17,35 @@ let device: string | number | boolean; // Union Type
 device = "Mobile";
 device = 4323523;
 
-const re: RegExp = /\w+/g
-
-
+const re: RegExp = /\w+/g;
 
 /* ------------------- 2. Function ------------------- */
 const sum = (a: number, b: number): number => {
-    return a + b;
-}
+  return a + b;
+};
+
+/* ------------------- 3. Array ------------------- */
+const anyTypeString = [];
+const anyTypeString2: any[] = [];
+
+const stringArr = ["Hello", "how", "are", "you"];
+const stringArr2: string[] = ["Hello", "how", "are", "you"];
+// X - stringArr.push(34)
+
+const strAndNum = ["Hi", 143];
+const strAndNum2: (string | number)[] = ["Hi", 143];
+// X - strAndNum2[0](boolean)
+
+const mixedData = ["bye", true, 77];
+const mixedData2: (string | boolean | number)[] = ["bye", true, 77];
+
+
+/* ------------------- 4. Tuple ------------------- */
+const myTuple: [string, number, boolean] = ['hello', 143, false]
+// X - const myTuple: [string, number, boolean, number] = ['hello', 143, false]
+let anArr = ['hello1', 234, true]
+
+myTuple[1] = 777
+
+anArr = myTuple
+// X - myTuple = Array
