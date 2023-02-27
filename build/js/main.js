526589cdf4e37f1915d6cccd4af506710b4679c6
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-27 08:50:17 +0600

    types & functions

--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,20 @@
 "use strict";
-let fullName = 'Riyad';
+/* ------------------- 1. Data Types ------------------- */
+let _id; // Implicit Declearation
+let fb_id; // Explicit Declearation
+fb_id = true;
+fb_id = 23423523;
+const firstName = "Riyad";
+const lastName = "Hossain";
+let isLoading;
+isLoading = false;
+let roll = 423;
+roll = "423";
+let device; // Union Type
+device = "Mobile";
+device = 4323523;
+const re = /\w+/g;
+/* ------------------- 2. Function ------------------- */
+const sum = (a, b) => {
+    return a + b;
+};
