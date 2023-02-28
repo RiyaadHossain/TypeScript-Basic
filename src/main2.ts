dfefdd90cc0f1059faed26bf0fffcab653459e67
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-28 15:52:46 +0600

    Interface

--- src/main2.ts
+++ src/main2.ts
@@ -1,4 +1,6 @@
 /* ------------------- 9. TS Classes ------------------- */
+
+// 9.1 Basic Class__________
 class Individual {
   //   name: string;
   //   age: number;
@@ -9,8 +11,8 @@ class Individual {
   constructor(
     public readonly name: string,
     readonly age: number,
-    private occupation: string, // accessible within class
-    protected exp: number  // accessible within class & subclasses
+    protected exp: number, // accessible within class & subclasses
+    private occupation: string // accessible within class
   ) {
     this.name = name;
     this.age = age;
@@ -18,32 +20,57 @@ class Individual {
     this.exp = exp;
   }
 
-  public getOccupation() {
+  getOccupation() {
     return `${this.name} is a ${this.occupation}`;
   }
 }
 
-const Riyad = new Individual("Riyad", 22, "St Engineer", 2);
+const Riyad = new Individual("Riyad", 22, 2, "St Engineer");
 console.log(Riyad.name);
 console.log(Riyad.getOccupation());
 // console.log(Riyad.occupation) - Property 'occupation' is private and only accessible within class 'Individual'
 // console.log(Riyad.exp) - Property 'exp' is protected and only accessible within class 'Individual' and its subclasses
 
+// 9.2 Subclass_________ 
 class Engineer extends Individual {
   constructor(
     name: string,
     age: number,
     occupation: string,
     exp: number,
-    readonly lang: string
+    readonly lang: string = "TypeScript" // Default Value
   ) {
-    super(name, age, occupation, exp);
+    super(name, age, exp, occupation);
     this.lang = lang;
   }
 
-  getExp() {
+  getExp(): string {
     return `I've ${this.exp} years of exp`;
   }
 }
 
+const SadikEngeer = new Engineer("Sadik", 25, "Std Engeer", 2);
+
+console.log(SadikEngeer.getExp());
+
+// 9.3 Interface__________
+interface Player {
+  name: string;
+  age: number;
+  play(action: string): string;
+}
+
+class Cricketer implements Player {
+  constructor(public name: string, public readonly age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  play(action: string): string {
+    return `${this.name} is ${action}`;
+  }
+}
+
+const Mashrafee = new Cricketer("Mashrafee", 32)
+console.log(Mashrafee.play("bowling")) // Mashrafee is bowling
 
